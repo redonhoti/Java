88d3ae9ad9397d5eb27d3eb31f77d00f9fd0f833
redonhoti <redonhoti20@gmail.com>
2025-02-14 13:58:29 -0800

    Create 14.java

---
+++ 14.java
@@ -0,0 +1,22 @@
+package projekt;
+
+public class shembull {
+
+	public static void main(String[] args) {
+	int N=10;
+	for(int i=0; i<N; i++) {
+		System.out.print("Pershendetje!");
+		for(int j=0; j<N; j++) {
+			System.out.print("Mirembrema!");
+			for(int k=0;k<N;k++) {
+				for(int l=0; l<N; l++) {
+					for(int m=0; m<N; m++) {
+						System.out.print("Unazat e nderthurura");
+				}
+			}
+		}
+	}
+
+}
+	}
+}

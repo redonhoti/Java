68a468c59fd03f4b77de19f4e861e2835015ff92
redonhoti <redonhoti20@gmail.com>
2025-02-14 14:28:34 -0800

    Create 18.java

---
+++ 18.java
@@ -0,0 +1,16 @@
+package projekt2;
+
+import java.util.Arrays;
+
+public class NumratNatyrore {
+
+	public static void main(String[] args) {
+	    int numratNatyrore[]=new int[50];
+	    for(int i=0; i<numratNatyrore.length; i++) {
+	    	numratNatyrore[i]=i;
+	    }
+	    	System.out.println(Arrays.toString(numratNatyrore));
+
+	}
+
+}

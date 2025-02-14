6ac83daf96b50a6844355638d364a2ddfd951d60
redonhoti <redonhoti20@gmail.com>
2025-02-14 14:12:29 -0800

    Create 16.java

---
+++ 16.java
@@ -0,0 +1,16 @@
+package projekt;
+
+public class mbledhVargun {
+
+public int mbledhVargun(int[] vargu) {
+	int  madhesia=vargu.length;
+	int shuma=0;
+	
+	for(int i=0; i<madhesia; i++) {
+		shuma +=vargu[i];
+	}
+	return shuma;
+
+	}
+
+}

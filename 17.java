607bfbc9d49504032ac9b2ba648e0a0f87e2622c
redonhoti <redonhoti20@gmail.com>
2025-02-14 14:20:02 -0800

    Create 17.java

---
+++ 17.java
@@ -0,0 +1,16 @@
+package projekt2;
+
+public class vargu {
+class Main{
+	public static void main(String[]args) {
+		int vargu[] = {1,2,3,4,5,6,7,8,9,10};
+		
+		for(int i=0; i<vargu.length; i++) {
+			System.out.println("Indeksi: "+i+";Elementi:" + 
+		                                     vargu[i]);
+		}
+	}
+
+	}
+
+}

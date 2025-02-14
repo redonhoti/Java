3ce8422ae171294dd11f241538750e94c63543c3
redonhoti <redonhoti20@gmail.com>
2025-02-14 13:42:31 -0800

    Create 12.java

---
+++ 12.java
@@ -0,0 +1,16 @@
+package projekt;
+
+public class UnazateNderthurura {
+
+	public static void main(String[] args) {
+		int N=6;
+		
+		for(int i=1; i<=N; i++) {
+			for(int j=1; j<=N; j++) {
+				System.out.println("i=" +i+ ", j=" + j);
+			}
+		}
+
+	}
+
+}

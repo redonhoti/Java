ff0e01931c426c5b1dda49b70bd6264979167e88
redonhoti <redonhoti20@gmail.com>
2025-02-14 13:09:35 -0800

    Create 1.java

---
+++ 1.java
@@ -0,0 +1,9 @@
+package projekt;
+import java.time.LocalDate;  // import the LocalDate class
+
+public class Main {  
+  public static void main(String[] args) {  
+    LocalDate myObj = LocalDate.now();  // Create a date object
+    System.out.println(myObj);  // Display the current date
+  }  
+}

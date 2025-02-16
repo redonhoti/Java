eb6d80f1392c77a6ebc40f5ca443c2200c0f50f3
redonhoti <redonhoti20@gmail.com>
2025-02-16 14:28:12 -0800

    Create 25.java

---
+++ 25.java
@@ -0,0 +1,15 @@
+package projekt2;
+
+public class FaktorielFor {
+
+	public static void main(String[] args) {
+	int n=4;
+	long faktoriel=1;
+	
+	for(int i=n; i>=1; i--) {
+		faktoriel *=1;
+	}
+System.out.println("Faktorieli i numrit "+n+" eshte "  + faktoriel);
+	}
+
+}
